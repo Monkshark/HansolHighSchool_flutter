4e8a6cbac0ed3cb92bdb193d95050ed6c4b16bfb
HuiDo Choo <manner0814@sjhansol.sjeduhs.kr>
2025-05-14 00:45:22 +0900

    Migration

--- lib/API/nies_api_keys.dart
+++
@@ -1,9 +0,0 @@
-class niesApiKeys {
-  static const nies_api_keys = 'db3161d360da4d07b650348a167d4411';
-  static const _ATPT_OFCDC_SC_CODE = 'I10';
-  static const _SD_SCHUL_CODE = '9300058';
-
-  static String get NIES_API_KEY => nies_api_keys;
-  static String get ATPT_OFCDC_SC_CODE => _ATPT_OFCDC_SC_CODE;
-  static String get SD_SCHUL_CODE => _SD_SCHUL_CODE;
-}
